f1ac277f10b9095f7da19e7dd4984b8c95a5464d
Riley <tudbury@gmail.com>
2020-06-08 09:12:24 -0600

    odd number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'databinding';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
 
   onIntervalFired(firedNumber: number) {
     console.log(firedNumber)
